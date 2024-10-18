0c0537b9ca311155eb2a394dfa203bb2b6c03ecc
affanmuhammed1234 <affanmuhammed2028@cs.sjcetpalai.ac.in>
2024-10-18 16:22:54 +0530

    Add files via upload

---
+++ affan3.py
@@ -0,0 +1,54 @@
+"""Write a Python program that demonstrates the usage of arithmetic, comparison, and logical operators. Perform a few operations and print the results.
+Instructions:
+
+    Perform addition and division using two integers (a = 10, b = 5).
+    Compare the two integers using the greater-than (>) and equality (==) operators.
+    Use logical operators (and, or) to check conditions.
+    Display the results of all the operations.
+
+Expected Output:
+
+Sum: 15, Division: 2.0
+
+Is a greater than b?: True
+
+Are a and b equal?: False
+
+Logical AND: True
+
+Logical OR: True"""
+
+number1 =int(input("enter the first number:"))
+number2 =int(input("enter the second  number:"))
+print("sum=",number1+number2,",Division=",number1/number2)
+print("Is number1 greater than number2?:",number1>number2)
+print("are number1 and number2 equal?:",number1==number2)
+print("logical AND:",number1>0 and b>0)
+print("logical OR:",number1>0 or b>10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
