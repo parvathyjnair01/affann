91f4e341033b3aea22ba1dff9fa1070b5a48b0a7
affanmuhammed1234 <affanmuhammed2028@cs.sjcetpalai.ac.in>
2024-10-25 15:13:52 +0530

    Add files via upload

---
+++ affan oct25 2.py
@@ -0,0 +1,10 @@
+num1 = int(input("enter the first number"))
+num2 = int(input("enter the second number number"))
+num3 = int(input("enter the third number"))
+if num1>num2 and num1>num3:
+    print(num1,"is greater than other two")
+elif num2>num3:
+    print(num2,"is greater than other two")
+else:
+    print(num3,"is greater than other two")
+
